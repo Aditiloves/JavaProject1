a511d346ed1b6428c3c7a300ad549602fa5d8dab
Aditiloves <aditi2019loves@gmail.com>
2019-02-17 03:00:04 -0500

    New peogram 2

--- pattern7.java
+++ pattern7.java
@@ -1,24 +1,22 @@
+import java.util.Scanner;
 
-public class pattern7
-{
-public static void main(String[] args)
-{
-	int i=7,j,k=7;
-	for(i=0;i<7;i++)
+
+public class starpattern1 {
+	public static void main(String args[])
 	{
-		System.out.print("\n");
+		Scanner sc=new Scanner (System.in);
+		int i,j,n;
+		System.out.println("Enter value for n :: ");
+		n=sc.nextInt();
 		
-		for(j=i+1;j<k;j++)
+		for(i=0;i<n;i++)
 		{
-			System.out.println(" ");
+			System.out.print("\n");
+			for(j=0;j<=i;j++)
+			{
+				System.out.print("*");
+			}
 		}
-		for(k=7;k>i;k--)
-		{
-			System.out.print("7");
-		}
-		
-		
 		
 	}
-}
 }
