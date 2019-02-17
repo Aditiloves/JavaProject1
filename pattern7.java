8082260eb1133ec9ce158fd2218996fbe45a3426
Aditiloves <aditi2019loves@gmail.com>
2019-02-17 02:42:52 -0500

    new Java Program

---
+++ pattern7.java
@@ -0,0 +1,24 @@
+
+public class pattern7
+{
+public static void main(String[] args)
+{
+	int i=7,j,k=7;
+	for(i=0;i<7;i++)
+	{
+		System.out.print("\n");
+		
+		for(j=i+1;j<k;j++)
+		{
+			System.out.println(" ");
+		}
+		for(k=7;k>i;k--)
+		{
+			System.out.print("7");
+		}
+		
+		
+		
+	}
+}
+}
