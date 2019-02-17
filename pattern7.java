603e96ae6d5283c08f5f70dd4aa2aa588120ec0f
Aditiloves <aditi2019loves@gmail.com>
2019-02-17 03:05:00 -0500

    Third TIme

--- pattern7.java
+++ pattern7.java
@@ -1,22 +1,24 @@
-import java.util.Scanner;
 
-
-public class starpattern1 {
-	public static void main(String args[])
+public class pattern7
+{
+public static void main(String[] args)
+{
+	int i=7,j,k=7;
+	for(i=0;i<7;i++)
 	{
-		Scanner sc=new Scanner (System.in);
-		int i,j,n;
-		System.out.println("Enter value for n :: ");
-		n=sc.nextInt();
+		System.out.print("\n");
 		
-		for(i=0;i<n;i++)
+		for(j=i+1;j<k;j++)
 		{
-			System.out.print("\n");
-			for(j=0;j<=i;j++)
-			{
-				System.out.print("*");
-			}
+			System.out.println(" ");
+		}
+		for(k=7;k>i;k--)
+		{
+			System.out.print("7");
 		}
 		
+		
+		
 	}
+}
 }
